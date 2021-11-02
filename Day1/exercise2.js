65ad32bf06aa9f782007ba7775ac1291e4b8b6e4
srisanjana0711 <67748695+srisanjana0711@users.noreply.github.com>
2021-11-02 12:49:31 +0530

    Add files via upload

---
+++ Day1/exercise2.js
@@ -0,0 +1,5 @@
+// write a JS program to get 2 numbers from user  and return true if one of the number is 50 or if sum is 50
+
+var x=Number(prompt("Enter First Number: "));
+var y=Number(prompt("Enter second Number: "));
+console.log(x==50 || y==50 || x+y==50 ? true : false);
---
+++ Day1/exercise3.js
@@ -0,0 +1,3 @@
+//JS Program to find length of string
+var s="I am Born to Win";
+console.log(s.length);
